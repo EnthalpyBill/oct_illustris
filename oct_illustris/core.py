0e9011c3ea647a40fa53562daa2cd13f35cc4c97
Bill Chen <ybchen@umich.edu>
2021-07-06 16:58:39 +0800

    Updated core

--- oct_illustris/core.py
+++ oct_illustris/core.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import h5py
 
 from .il_util import partTypeNum
 from .loader import loadFile
@@ -14,10 +15,15 @@ class dataset(object):
         self._pt = pt
         self._height = height
 
+        self._pt_idx = 0
+
+        for p in pt:
+            self._pt_idx += 2**partTypeNum(p)
+
         if index_fn:
             self._index_fn = index_fn
         else:
-            self._index_fn = fn + ".idx_h%02d.h5"%(height)
+            self._index_fn = fn + ".idx_h%02d_pt%02d.h5"%(height, self._pt_idx)
 
         self._index = None
         with h5py.File(fn, 'r') as f:
@@ -45,24 +51,48 @@ class dataset(object):
     @property
     def index(self):
         if self._index:
-            return 1
-
-        if os.path.isfile(self._index_fn):
-            # Load index
-            return 1
+            return self._index
 
         # Generate index
+        self._index = {}
+
+        # Load index if index file exists
+        if os.path.isfile(self._index_fn):
+            with h5py.File(self._index_fn,'r') as f:
+                for p in self._pt:
+                    ptNum = partTypeNum(p)
+                    gName = "PartType%d"%(ptNum)
+                    self._index[gName]["count"] = f[gName].attrs["count"]
+
+                    if not self._index[gName]["count"]:
+                        continue
+
+                    self._index[gName]["index"] = f[gName]["index"][:]
+
+            return self._index
+
+        # Compute and save index if index file does not exist
         data = loadFile(fn, self._pt, "Coordinates")
+        with h5py.File(self._index_fn,'w') as f:
+            # Loop over each requested field for this particle type
+            for p in self._pt:
+                ptNum = partTypeNum(p)
+                gName = "PartType%d"%(ptNum)
+                grp = f.create_group(gName)
 
-        # Loop over each requested field for this particle type
-        for i, field in enumerate(fields):
-            ptNum = partTypeNum(pt)
-            gName = "PartType%01d"%(ptNum)
-            length = data[gName]["count"]
-            if not length:
-                continue
+                length = data[gName]["count"]
+                result[gName]["count"] = length
+                grp.attrs["count"] =  length
 
-            pos = data[gName]["Coordinates"]
-            ot = octree(pos, length, 0, self._boundary, self._height)
-            idx = ot.build()
+                if not length:
+                    continue
+
+                pos = data[gName]["Coordinates"]
+                ot = octree(pos, length, 0, self._boundary, self._height)
+
+                self._index[gName]["index"] = ot.build()
+                grp.create_dataset("index", 
+                    data=self._index[gName]["index"], dtype="u4")
+
+        return self._index
 
