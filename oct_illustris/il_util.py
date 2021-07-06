4f62f8236fb61aebd3be2b428ad3f057b6036df0
Bill Chen <ybchen@umich.edu>
2021-07-06 16:58:48 +0800

    Updated il_util

--- oct_illustris/il_util.py
+++ oct_illustris/il_util.py
@@ -5,34 +5,34 @@ def partTypeNum(partType):
     if str(partType).isdigit():
         return int(partType)
         
-    if str(partType).lower() in ['gas','cells']:
+    if str(partType).lower() in ["gas","cells"]:
         return 0
-    if str(partType).lower() in ['dm','darkmatter']:
+    if str(partType).lower() in ["dm","darkmatter"]:
         return 1
-    if str(partType).lower() in ['tracer','tracers','tracermc','trmc']:
+    if str(partType).lower() in ["tracer","tracers","tracermc","trmc"]:
         return 3
-    if str(partType).lower() in ['star','stars','stellar']:
+    if str(partType).lower() in ["star","stars","stellar"]:
         return 4 # only those with GFM_StellarFormationTime>0
-    if str(partType).lower() in ['wind']:
+    if str(partType).lower() in ["wind"]:
         return 4 # only those with GFM_StellarFormationTime<0
-    if str(partType).lower() in ['bh','bhs','blackhole','blackholes']:
+    if str(partType).lower() in ["bh","bhs","blackhole","blackholes"]:
         return 5
     
     raise Exception("Unknown particle type name.")
 
 def snapPath(basePath, snapNum, chunkNum=0):
     """ Return absolute path to a snapshot HDF5 file (modify as needed). """
-    snapPath = basePath + '/snapdir_' + str(snapNum).zfill(3) + '/'
-    filePath = snapPath + 'snap_' + str(snapNum).zfill(3)
-    filePath += '.' + str(chunkNum) + '.hdf5'
+    snapPath = basePath + "/snapdir_%03d/"%(snapNum)
+    filePath = snapPath + "snap_%03d.%d.hdf5"%(snapNum, chunkNum)
     return filePath
 
 def getNumPart(header):
     """ Calculate number of particles of all types given a snapshot header. """
     nTypes = 6
 
-    nPart = np.zeros(nTypes, dtype='i8')
+    nPart = np.zeros(nTypes, dtype="u4")
     for j in range(nTypes):
-        nPart[j] = header['NumPart_Total'][j] | (header['NumPart_Total_HighWord'][j] << 32)
+        nPart[j] = (header["NumPart_Total"][j] | 
+            (header["NumPart_Total_HighWord"][j] << 32))
 
     return nPart
