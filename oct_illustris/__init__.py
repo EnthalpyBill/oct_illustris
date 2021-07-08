d33127f8655e63cd7ac5efeea38d2af97743a167
Bill Chen <ybchen@umich.edu>
2021-07-08 21:37:50 +0800

    Updated __init__

--- oct_illustris/__init__.py
+++ oct_illustris/__init__.py
@@ -1,3 +1,10 @@
+from . import core
+from . import il_util
+from . import loader
+from . import octree
+
+from .loader import load
+
 __all__ = ["core", "il_util", "loader", "octree"]
 __version__ = "0.0.dev"
 __name__ = "oct_illustris"
