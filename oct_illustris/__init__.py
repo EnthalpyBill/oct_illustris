14f30d020295dfe61c8e358ab94ab28007c26405
Bill Chen <ybchen@umich.edu>
2021-07-24 23:03:28 +0800

    Updated __init__

--- oct_illustris/__init__.py
+++ oct_illustris/__init__.py
@@ -2,12 +2,14 @@
 # License: MIT (https://opensource.org/licenses/MIT)
 
 from . import core, il_util, loader, octree
+
+__all__ = core.__all__ + il_util.__all__ + loader.__all__ + octree.__all__
+
 from .core import *
 from .il_util import *
 from .loader import *
 from .octree import *
 
-__all__ = core.__all__ + il_util.__all__ + loader.__all__ + octree.__all__
 __version__ = "0.0.dev"
 __name__ = "oct_illustris"
 __author__ = ["Bill Chen"]
